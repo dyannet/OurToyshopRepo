61cb9c580867a5d492e81dfe2e35dcb0f511681e
patgel <pgellon@gmail.com>
2012-07-17 00:48:37 +0100

    search changes

--- OurToyShop/toyshop/app/models/product.rb
+++ OurToyShop/toyshop/app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   def self.search(search_query)
     if search_query
-      find(:all,:conditions => ['name LIKE ?', "%#{search_query}%"])
+      find(:all,:conditions => ['title LIKE ?', "%#{search_query}%"])
     else
       find(:all)
     end

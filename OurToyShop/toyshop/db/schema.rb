61d3f44e7494afe2a48e388eb547d880adf80dfe
Dyanne Tuite <dyannetuite@yahoo.com>
2012-07-17 00:29:11 +0100

    Tidy Up

--- OurToyShop/toyshop/db/schema.rb
+++ OurToyShop/toyshop/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716110240) do
+ActiveRecord::Schema.define(:version => 20120716152341) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120716110240) do
     t.string   "category"
     t.string   "age"
     t.integer  "user_id"
+    t.string   "fake"
   end
 
 end

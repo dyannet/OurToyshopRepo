5fd17be3762e9307cadfd388abc4d98e9e54d787
patgel <pgellon@gmail.com>
2012-07-18 13:40:38 +0100

    deleted

--- OurToyShop/toyshop/db/migrate/20120717192418_add_admin_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddAdminToUser < ActiveRecord::Migration
-  def self.up
-    add_column :users,:admin, :bolean,:default => false
-  end
-
-  def self.down
-    remove column :users, :admin
-  end
-end
